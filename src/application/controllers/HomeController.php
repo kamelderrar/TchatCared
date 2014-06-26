a46cde53cd072694b72250a57b4f2895363fad66
sferey <s.ferey@cleo-consulting.fr>
2014-06-26 14:43:28 +0200

    correctif

--- src/application/controllers/HomeController.php
+++ src/application/controllers/HomeController.php
@@ -10,7 +10,5 @@ class HomeController extends Controller
     public function action()
     {
         $this->view->auth = $this->request->getSession()->getNamespace('auth');
-        
-        
     }
 }

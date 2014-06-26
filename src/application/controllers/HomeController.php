b07dc3c694f3ea3820a8d517b424944baba33095
Linaida <dev.linda66@gmail.com>
2014-06-26 15:22:58 +0200

    ajout password + submit

--- src/application/controllers/HomeController.php
+++ src/application/controllers/HomeController.php
@@ -10,5 +10,10 @@ class HomeController extends Controller
     public function action()
     {
         $this->view->auth = $this->request->getSession()->getNamespace('auth');
+        $methode = $this->request->getMethod();
+        $form_data = $this->request->getParams();
+        if($methode=="POST"){
+        	var_dump($this->request->getParams());
+        }
     }
 }

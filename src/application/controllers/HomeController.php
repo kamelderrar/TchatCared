bb75eca3fb058a49af21d971e4e87737a60780ac
Linaida <dev.linda66@gmail.com>
2014-06-26 15:54:05 +0200

    Verification du formulaire

--- src/application/controllers/HomeController.php
+++ src/application/controllers/HomeController.php
@@ -7,13 +7,37 @@
  */
 class HomeController extends Controller
 {
+	public function verifForm(){
+		$methode = $this->request->getMethod();
+		$form_data = array();
+		$form_data = $this->request->getParams();
+		$errmsg = array();
+		$msg= "";
+		if(isset($form_data['login']) && !empty($form_data['login'])){
+			$msg= $msg."||".$form_data['login'];
+		}else{
+			$errmsg[]= "Champs login vide";
+		}
+		if(isset($form_data['password']) && !empty($form_data['password'])){
+			$msg= $msg."||".$form_data['password'];
+		}else{
+			$errmsg[]= "Champs password vide";
+		}
+		if(empty($errmsg)){
+			return $msg;
+		}
+		else{
+			return $errmsg;
+		}
+	}
+	
     public function action()
     {
         $this->view->auth = $this->request->getSession()->getNamespace('auth');
-        $methode = $this->request->getMethod();
-        $form_data = $this->request->getParams();
-        if($methode=="POST"){
-        	var_dump($this->request->getParams());
-        }
+       
+        print_r($this->verifForm());
+       
+      }
+    
     }
-}
+

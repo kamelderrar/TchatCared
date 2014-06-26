716621f2981cc49b41f8af3788ed3d90b5e2901a
Pandious <lebillant.loic@gmail.com>
2014-06-26 17:00:39 +0200

    Recup Z

--- src/application/controllers/HomeController.php
+++ src/application/controllers/HomeController.php
@@ -9,6 +9,26 @@ class HomeController extends Controller
 {
     public function action()
     {
-        $this->view->auth = $this->request->getSession()->getNamespace('auth');
+    	$this->view->auth = $this->request->getSession()->getNamespace('auth');
+    	
+    	print_r($this->verificationForm ());
     }
+    
+    public function verificationForm()
+    {
+    	$post=$_POST;
+    	
+    	if(isset($post['login']) && isset($post['password']))
+    	{
+    		if(!empty($_POST['login']) && !empty($_POST['password'])){
+    			$this->view->auth = $this->request->getSession()->getNamespace('auth');
+    			echo $post['login'];
+    		}
+    		else
+    		{
+    			echo 'Veuillez renseigner tous les champs.';
+    		}
+    	}
+    }
+    
 }

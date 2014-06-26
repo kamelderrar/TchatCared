90543cf2218abfc73013c91d3253493d646d30fe
Linaida <dev.linda66@gmail.com>
2014-06-26 16:51:56 +0200

    Modification de la verification

--- src/application/controllers/HomeController.php
+++ src/application/controllers/HomeController.php
@@ -5,39 +5,34 @@
  * @author Formateur
  *
  */
-class HomeController extends Controller
-{
-	public function verifForm(){
-		$methode = $this->request->getMethod();
-		$form_data = array();
-		$form_data = $this->request->getParams();
-		$errmsg = array();
-		$msg= "";
-		if(isset($form_data['login']) && !empty($form_data['login'])){
-			$msg= $msg."||".$form_data['login'];
-		}else{
-			$errmsg[]= "Champs login vide";
+class HomeController extends Controller {
+	public function verifForm() {
+		$methode = $this->request->getMethod ();
+		$form_data = array ();
+		$form_data = $this->request->getParams ();
+		$errmsg = array ();
+		$msg = "";
+		
+		if (isset ( $form_data ['login'] ) && ! empty ( $form_data ['login'] )) {
+			$msg = $msg . "||" . $form_data ['login'];
+		} else {
+			$errmsg [] = "Champs login vide";
 		}
-		if(isset($form_data['password']) && !empty($form_data['password'])){
-			$msg= $msg."||".$form_data['password'];
-		}else{
-			$errmsg[]= "Champs password vide";
+		if (isset ( $form_data ['password'] ) && ! empty ( $form_data ['password'] )) {
+			$msg = $msg . "||" . $form_data ['password'];
+		} else {
+			$errmsg [] = "Champs password vide";
 		}
-		if(empty($errmsg)){
+		if (empty ( $errmsg )) {
 			return $msg;
-		}
-		else{
+		} else {
 			return $errmsg;
 		}
 	}
-	
-    public function action()
-    {
-        $this->view->auth = $this->request->getSession()->getNamespace('auth');
-       
-        print_r($this->verifForm());
-       
-      }
-    
-    }
+	public function action() {
+		$this->view->auth = $this->request->getSession ()->getNamespace ( 'auth' );
+		
+		print_r($verification=$this->verifForm());
+	}
+}
 
